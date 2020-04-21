a3c0333cb1cc33c433b8e813ebd81f6063f6adb9
TrapSack <61499327+TrapSack@users.noreply.github.com>
2020-04-21 16:32:04 +0300

    wrong code uploaded

--- AMPProject/2.2.1.cpp
+++ AMPProject/2.2.1.cpp
@@ -1,8 +1,33 @@
-#include <QCoreApplication>
+#include <stdio.h>
+#include <math.h>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+long long countFactorial (long long number) {
+    if (number <= 1) {
+        return 1;
+    } else return number*countFactorial(number - 1);
 }
+int checkAccuracy(double x, double err) {
+    double sum = x, result = 1;
+   
+    while (fabs(sin(x) - sum) >= err) {
+        sum += pow(-1, result - 1) * (pow(x, 2*result - 1)/countFactorial(2*result - 1));
+        result++;
+    }
+    return result;
+}
+
+int main() {
+    double x, error;
+    int result;
+    
+    printf("Pogreshnost: ");
+    scanf("%lfouble", &error);
+    fflush(stdin);
+    printf("x: ");
+    scanf("%lfouble", &x);
+    result = checkAccuracy(x, error);
+    printf("%s%d%s", "if number is = ", result, " it`s your accuracy\n");
+}
+
+
+
